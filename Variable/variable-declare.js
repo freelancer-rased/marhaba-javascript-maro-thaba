a5b8232d27119dac21c3f733aefc129414f80e1c
freelancer-rased <mdraseduzzamanrased8707@gmail.com>
2025-02-21 19:48:44 +0600

    variable declare practice complete

---
+++ Variable/variable-declare.js
@@ -0,0 +1,20 @@
+// 1. Put your DOB in a variable
+const myBirthYear = 1998;
+
+// 2. How many siblings do you have? write in a variable 
+const siblings = 4;
+
+// 3. How many student are there in your class? Declare a variable the name of students
+const students = 120;
+
+// 4. How much mark you got in your last exam? write that in a marks variable
+const marks = 98;
+
+// 5. how many room in your home? write that in a rooms variable 
+const rooms = 5;
+
+// 6. write your daily study hour in a variable 
+const studyHour = 8;
+
+// 7. Phone number total digit after addition 
+const totalAddition = 79;
