b70806d06275ff1cc4ce427d7e679e9fae911fb7
freelancer-rased <mdraseduzzamanrased8707@gmail.com>
2025-02-21 20:01:00 +0600

    print variable practice done

---
+++ Variable/print-variable.js
@@ -0,0 +1,27 @@
+// 1. print your age in a variable 
+const age = 28;
+console.log(age);
+
+// 2. write your favorite book name in a booksToRead variable and print that 
+const booksToRead = 30;
+console.log(booksToRead);
+
+// 3. write your wake time in a wakeUpTime variable 
+const wakeUpTime = 8;
+console.log(wakeUpTime);
+
+// 4. print your wasted time in wastedTime variable
+const wastedTime = 1;
+console.log(wastedTime);
+
+// 5. print how much you have borrow 
+const borrow = 500;
+console.log(borrow);
+
+// 6. print your favorite team given highest goal 
+const goals = 7;
+console.log(goals);
+
+// 7. write how much pillow do you have in your bad
+const pillow = 2;
+console.log(pillow);
